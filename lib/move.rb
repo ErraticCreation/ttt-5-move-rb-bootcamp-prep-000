4cb026ebb238e392e533c3f9c07d22e419903d18
Tarah Nixon <tarah.sage.nixon@gmail.com>
2018-04-06 03:06:58 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,6 +11,6 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move (board_array, board_index, player_character = " ")
+def move (board_array, board_index, player_character = "X")
   board_array[board_index] = player_character
 end
